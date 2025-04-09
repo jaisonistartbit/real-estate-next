f804f1bf048e7397c62979b2a598dec82f93f4e5
jai <jaisoni@startbitsolutions.com>
2025-04-09 16:51:32 +0530

    Update route.js

--- app/api/graphql/route.js
+++ app/api/graphql/route.js
@@ -1,7 +1,8 @@
-import { ApolloServer } from '@apollo/server';
-import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { typeDefs } from '@/graphql/typeDefs';
 import { resolvers } from '@/graphql/resolvers';
+import { ApolloServer } from '@apollo/server';
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+
 
 const server = new ApolloServer({
   typeDefs,
