28c88e8fd734ae990f0687a0e8268a4760266df1
JaiSoniSB <149651763+jaisonistartbit@users.noreply.github.com>
2025-05-30 16:06:59 +0530

    Update NearCities.jsx

--- components/users/landing_page/neighbour-cities/NearCities.jsx
+++ components/users/landing_page/neighbour-cities/NearCities.jsx
@@ -50,16 +50,16 @@ const NearCities = () => {
 
                 <div className="w-[93%] mx-auto mt-[50px] mb-[50px]">
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 my-3">
-                        {cities?.[0] && <div onClick={() => { router.push(`/city/${cities?.[0]?.city}`) }} className="text-center"><CityCardView imageUrl={'/images/cities/city1.jpg'} cityName={cities?.[0]?.city} propertyCount={cities?.[0]?.no_of_property} /></div>}
-                        {cities?.[1] && <div onClick={() => { router.push(`/city/${cities?.[1]?.city}`) }} className="text-center"><CityCardView imageUrl={'/images/cities/city5.jpg'} cityName={cities?.[1]?.city} propertyCount={cities?.[1]?.no_of_property} /></div>}
-                        {cities?.[2] && <div onClick={() => { router.push(`/city/${cities?.[2]?.city}`) }} className="text-center"><CityCardView imageUrl={'/images/cities/city3.jpg'} cityName={cities?.[2]?.city} propertyCount={cities?.[2]?.no_of_property} /></div>}
-                        {cities?.[3] && <div onClick={() => { router.push(`/city/${cities?.[3]?.city}`) }} className="text-center"><CityCardView imageUrl={'/images/cities/city4.jpg'} cityName={cities?.[3]?.city} propertyCount={cities?.[3]?.no_of_property} /></div>}
+                        {cities?.[0] && <div onClick={() => { router.push(`/city/${cities?.[0]?.city}`) }} className="text-center cursor-pointer"><CityCardView imageUrl={'/images/cities/city1.jpg'} cityName={cities?.[0]?.city} propertyCount={cities?.[0]?.no_of_property} /></div>}
+                        {cities?.[1] && <div onClick={() => { router.push(`/city/${cities?.[1]?.city}`) }} className="text-center cursor-pointer"><CityCardView imageUrl={'/images/cities/city5.jpg'} cityName={cities?.[1]?.city} propertyCount={cities?.[1]?.no_of_property} /></div>}
+                        {cities?.[2] && <div onClick={() => { router.push(`/city/${cities?.[2]?.city}`) }} className="text-center cursor-pointer"><CityCardView imageUrl={'/images/cities/city3.jpg'} cityName={cities?.[2]?.city} propertyCount={cities?.[2]?.no_of_property} /></div>}
+                        {cities?.[3] && <div onClick={() => { router.push(`/city/${cities?.[3]?.city}`) }} className="text-center cursor-pointer"><CityCardView imageUrl={'/images/cities/city4.jpg'} cityName={cities?.[3]?.city} propertyCount={cities?.[3]?.no_of_property} /></div>}
                     </div>
 
                     <div className="grid grid-cols-4 gap-4">
-                        {cities?.[4] && <div onClick={() => { router.push(`/city/${cities?.[4]?.city}`) }} className="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1 text-center"><CityCardView imageUrl={'/images/cities/city2.jpg'} cityName={cities?.[4]?.city} propertyCount={cities?.[4]?.no_of_property} /></div>}
-                        {cities?.[5] && <div onClick={() => { router.push(`/city/${cities?.[5]?.city}`) }} className="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-2 text-center"><CityCardView imageUrl={'/images/cities/city6.jpg'} cityName={cities?.[5]?.city} propertyCount={cities?.[5]?.no_of_property} /></div>}
-                        {cities?.[6] && <div onClick={() => { router.push(`/city/${cities?.[6]?.city}`) }} className="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1 text-center"><CityCardView imageUrl={'/images/cities/city7.jpg'} cityName={cities?.[6]?.city} propertyCount={cities?.[6]?.no_of_property} /></div>}
+                        {cities?.[4] && <div onClick={() => { router.push(`/city/${cities?.[4]?.city}`) }} className="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1 text-center cursor-pointer"><CityCardView imageUrl={'/images/cities/city2.jpg'} cityName={cities?.[4]?.city} propertyCount={cities?.[4]?.no_of_property} /></div>}
+                        {cities?.[5] && <div onClick={() => { router.push(`/city/${cities?.[5]?.city}`) }} className="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-2 text-center cursor-pointer"><CityCardView imageUrl={'/images/cities/city6.jpg'} cityName={cities?.[5]?.city} propertyCount={cities?.[5]?.no_of_property} /></div>}
+                        {cities?.[6] && <div onClick={() => { router.push(`/city/${cities?.[6]?.city}`) }} className="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1 text-center cursor-pointer"><CityCardView imageUrl={'/images/cities/city7.jpg'} cityName={cities?.[6]?.city} propertyCount={cities?.[6]?.no_of_property} /></div>}
                     </div>
                 </div>
             </div>
