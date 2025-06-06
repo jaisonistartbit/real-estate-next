ee8a98ba2f7ba482658149e2b65bc5cda1b0b46e
yashwantgolechastartbit <149651763+jaisonistartbit@users.noreply.github.com>
2025-06-06 17:11:26 +0530

    commit

--- app/auth/callback/page.js
+++ app/auth/callback/page.js
@@ -3,8 +3,8 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
